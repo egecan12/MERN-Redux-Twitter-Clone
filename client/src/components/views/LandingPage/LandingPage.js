47fdff687327818592f7aec2241d7bbff01d8422
egecan <egecan18k@gmail.com>
2021-02-24 18:01:00 -0500

    first commit

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 import { FaCode } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+
+function LandingPage(props) {
+
+  const data = useSelector((state) => state.user.userData);
+  console.log(data?.email);
+  
+  
+  
+  return (
+    <> 
+      <div className="app">
+        <FaCode style={{ fontSize: "4rem" }} />
+        <br />
+        <span style={{ fontSize: "2rem" }}>Let's Start Coding!</span>
+      </div>
+      
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
